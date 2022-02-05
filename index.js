dbee5290c51a69434b77b80d98807778e483b4b5
Brian Arnold <b.arnold09@gmail.com>
2022-02-05 13:37:16 -0500

    commit tree a3964f6bc2f4e975c223aaf317c6c032657af362

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find(object => object.result === "W"); // find object with result: "W"
+    if (result) {
+        return result.year // select year from object
+    } else return undefined
+    
+}
